154b8b422eefce285e9efdd3f409ec8be03c9a3b
Amos Ndonga <amosndonga@gmail.com>
2022-08-31 21:49:16 +0300

    finished working on index page

--- pages/components/Videos.js
+++ pages/components/Videos.js
@@ -1,8 +1,11 @@
 import { Stack, Box } from '@mui/material';
 import ChannelCard from './ChannelCard';
 import VideoCard from './VideoCard';
+import Loader from './Loader';
 
 function Videos({ videos, direction }) {
+  if (!videos?.length) return <Loader />;
+  
   return (
     <Stack
       direction={direction || 'row'}
